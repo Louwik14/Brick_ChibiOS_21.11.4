20c89f0a8df85ee03ef1b380e2da09a3b801cde2
Louwik14 <loicdu14150@hotmail.fr>
2025-12-31 13:22:52 +0100

    modif

--- demos/STM32/RT-STM32H743ZI_REV_XY-NUCLEO144/main.c
+++ demos/STM32/RT-STM32H743ZI_REV_XY-NUCLEO144/main.c
@@ -1,23 +1,20 @@
 #include "ch.h"
 #include "hal.h"
+#include "stm32h7xx.h"
 
 int main(void) {
 
   halInit();
   chSysInit();
 
-  /* PH7 : LED active-low
-     Forcer mode + état initial connu */
-  palSetPadMode(GPIOH, 7, PAL_MODE_OUTPUT_PUSHPULL);
-  palSetPad(GPIOH, 7);   /* LED OFF */
+  RCC->AHB4ENR |= RCC_AHB4ENR_GPIOHEN;
 
-  while (true) {
-    /* LED ON */
-    palClearPad(GPIOH, 7);
-    chThdSleepMilliseconds(1000);
+  GPIOH->MODER &= ~(3U << (7*2));
+  GPIOH->MODER |=  (1U << (7*2));   // output
+  GPIOH->OTYPER &= ~(1U << 7);
 
-    /* LED OFF */
-    palSetPad(GPIOH, 7);
-    chThdSleepMilliseconds(1000);
+  while (1) {
+    GPIOH->ODR ^= (1U << 7);
+    for (volatile uint32_t i = 0; i < 1000000; i++);
   }
 }
