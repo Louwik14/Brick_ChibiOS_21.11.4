9d5605ed197ca3ec6dc8187394f6daae8da9d1e9
Louwik14 <loicdu14150@hotmail.fr>
2026-01-03 21:54:20 +0100

    Simplify USB MIDI demo

--- demos/STM32/RT-STM32H743ZI_REV_XY-NUCLEO144/main.c
+++ demos/STM32/RT-STM32H743ZI_REV_XY-NUCLEO144/main.c
@@ -1,82 +1,23 @@
 #include "ch.h"
 #include "hal.h"
-#include "drv_display.h"
-
-/* ===================== RANDOM WAVE ===================== */
-
-#define WAVE_WIDTH    128
-#define WAVE_CENTER_Y 32
-
-static int8_t wave[WAVE_WIDTH];
-
-/* PRNG simple et rapide */
-static uint32_t rng = 0x12345678;
-
-static int8_t rand8(void) {
-    rng ^= rng << 13;
-    rng ^= rng >> 17;
-    rng ^= rng << 5;
-    return (int8_t)(rng & 0x7F) - 64;
-}
-
-/* Initialise une onde douce */
-static void init_wave(void) {
-    int8_t v = 0;
-    for (int i = 0; i < WAVE_WIDTH; i++) {
-        v += rand8() >> 3;      /* lissage */
-        if (v > 32)  v = 32;
-        if (v < -32) v = -32;
-        wave[i] = v;
-    }
-}
-
-/* ===================== DRAW ===================== */
-
-static void draw_wave(void) {
-
-    for (int x = 0; x < WAVE_WIDTH; x++) {
-
-        int y = WAVE_CENTER_Y - wave[x];
-
-        /* onde épaisse */
-        drv_display_draw_pixel(x, y - 1, true);
-        drv_display_draw_pixel(x, y,     true);
-        drv_display_draw_pixel(x, y + 1, true);
-    }
-}
-
-/* ===================== MAIN ===================== */
+#include "midi.h"
+#include "usb_device.h"
 
 int main(void) {
+  halInit();
+  chSysInit();
 
-    halInit();
-    chSysInit();
+  usb_device_start();
+  midi_init();
 
-    drv_display_start();
-    init_wave();
+  while (!usb_device_active()) {
+    chThdSleepMilliseconds(10);
+  }
 
-    while (true) {
-
-        drv_display_clear();
-
-        drv_display_draw_text(0, 0, "NOISE WAVE");
-
-        draw_wave();
-
-        /* === animation === */
-
-        /* décale tout à gauche */
-        for (int i = 0; i < WAVE_WIDTH - 1; i++) {
-            wave[i] = wave[i + 1];
-        }
-
-        /* génère une nouvelle valeur */
-        int8_t v = wave[WAVE_WIDTH - 2];
-        v += rand8() >> 3;
-        if (v > 32)  v = 32;
-        if (v < -32) v = -32;
-        wave[WAVE_WIDTH - 1] = v;
-
-        chThdSleepMilliseconds(33); /* ~30 FPS */
-    }
+  while (true) {
+    midi_note_on(MIDI_DEST_USB, 0, 60, 100);
+    chThdSleepMilliseconds(1000);
+    midi_note_off(MIDI_DEST_USB, 0, 60, 0);
+    chThdSleepMilliseconds(1000);
+  }
 }
