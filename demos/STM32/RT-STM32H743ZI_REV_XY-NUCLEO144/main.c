2d179634918ef57f958e174be96e85003bae9125
Louwik14 <loicdu14150@hotmail.fr>
2026-01-15 22:15:32 +0100

    Revert "Merge pull request #137 from Louwik14/codex/analyze-sdmmc-driver-initialization-in-chibios"
    This reverts commit a1efb3c22486c82fb94804e1a23bd796433a3a6f, reversing
    changes made to 57e48266a3ba9579d487fc8ca95a61ca62399416.

--- demos/STM32/RT-STM32H743ZI_REV_XY-NUCLEO144/main.c
+++ demos/STM32/RT-STM32H743ZI_REV_XY-NUCLEO144/main.c
@@ -5,8 +5,6 @@
 /* Buffer MUST be aligned and non-cached on H7 */
 __attribute__((section(".nocache"), aligned(32)))
 static uint8_t txbuf[512];
-__attribute__((section(".nocache"), aligned(32)))
-static uint8_t rxbuf[512];
 
 int main(void) {
 
@@ -28,7 +26,6 @@ int main(void) {
   for (int i = 0; i < 512; i++) {
     txbuf[i] = 0xA5;
   }
-  memset(rxbuf, 0, sizeof(rxbuf));
 
   /* Write block 0 */
   if (sdcWrite(&SDCD1, 0, txbuf, 1)) {
@@ -37,25 +34,6 @@ int main(void) {
       chThdSleepMilliseconds(1000);
     }
   }
-
-  /* Read back block 0 */
-  if (sdcRead(&SDCD1, 0, rxbuf, 1)) {
-    /* Read failed: hang here */
-    while (1) {
-      chThdSleepMilliseconds(1000);
-    }
-  }
-
-  /* Compare data */
-  if (memcmp(txbuf, rxbuf, sizeof(txbuf)) != 0) {
-    /* Mismatch: hang here */
-    while (1) {
-      chThdSleepMilliseconds(1000);
-    }
-  }
-
-  /* Disconnect card after test */
-  sdcDisconnect(&SDCD1);
 
   /* Done: stay here forever */
   while (1) {
--- os/hal/ports/STM32/LLD/SDMMCv2/hal_sdc_lld.c
+++ os/hal/ports/STM32/LLD/SDMMCv2/hal_sdc_lld.c
@@ -468,8 +468,9 @@ void sdc_lld_stop(SDCDriver *sdcp) {
 void sdc_lld_start_clk(SDCDriver *sdcp) {
 
   /* Initial clock setting: 400kHz, 1bit mode.*/
-  sdcp->sdmmc->CLKCR  = sdc_lld_clkdiv(sdcp, 400000U) | SDMMC_CLKCR_CLKEN;
+  sdcp->sdmmc->CLKCR  = sdc_lld_clkdiv(sdcp, 400000U);
   sdcp->sdmmc->POWER |= SDMMC_POWER_PWRCTRL_0 | SDMMC_POWER_PWRCTRL_1;
+/* TODO sdcp->sdmmc->CLKCR |= SDMMC_CLKCR_CLKEN;*/
 
   /* Clock activation delay.*/
   osalThreadSleep(OSAL_MS2I(STM32_SDC_SDMMC_CLOCK_DELAY));
